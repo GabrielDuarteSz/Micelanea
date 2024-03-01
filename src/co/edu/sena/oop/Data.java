9b66221b89e9b2040367092baeb6aed1878e912d
GabrielDuarteSz <160949764+GabrielDuarteSz@users.noreply.github.com>
2024-02-29 21:26:42 -0500

    Add files via upload

---
+++ src/co/edu/sena/oop/Data.java
@@ -0,0 +1,12 @@
+package co.edu.sena.oop;
+
+public class Data {
+    public double areaTriangulo(double num1, double num2){
+        return (num1 * num2) / 2;
+
+    }
+    public double sumarDosnumeros(double num1, double num2){
+        return (num1 + num2);
+    }
+
+}
